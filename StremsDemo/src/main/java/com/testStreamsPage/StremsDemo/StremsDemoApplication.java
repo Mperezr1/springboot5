852d833ea5ba101d0abada62549ed996e8f88654
manolo <manuelestebanpr@gmail.com>
2020-08-07 09:36:44 -0500

    First Commit demo app spring framework 5

---
+++ StremsDemo/src/main/java/com/testStreamsPage/StremsDemo/StremsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.testStreamsPage.StremsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StremsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StremsDemoApplication.class, args);
+	}
+
+}
---
+++ StremsDemo/src/test/java/com/testStreamsPage/StremsDemo/StremsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testStreamsPage.StremsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StremsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
